5af9d61deadfce47f921ad88f6184f11aed96fcf
Yana Rudometova <rudometova.ya@gmail.com>
2025-07-11 00:21:02 +0300

    add modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,6 +6,11 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import { fetchMovies } from "../../services/movieService";
 import Pagination from "../Pagination/Pagination";
 
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Loader from "../Loader/Loader";
+import MovieModal from "../MovieModal/MovieModal";
+import type { Movie } from "../../types/movie";
+
 export default function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [topic, setTopic] = useState("");
@@ -15,6 +20,12 @@ export default function App() {
     enabled: topic !== "",
     placeholderData: keepPreviousData,
   });
+
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+  };
 
   const totalPage = data?.total_pages ?? 0;
 
@@ -34,15 +45,18 @@ export default function App() {
           setPage={setCurrentPage}
         />
       )}
-      {isLoading && <strong>Loading articles...</strong>}
-      {isError && <strong>Oops, there was an error...</strong>}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       {data && data.results.length > 0 && (
         <MovieGrid
           movies={data.results}
           onSelect={(movie) => {
-            console.log("Selected movie:", movie);
+            setSelectedMovie(movie);
           }}
         />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={closeModal} />
       )}
     </>
   );
